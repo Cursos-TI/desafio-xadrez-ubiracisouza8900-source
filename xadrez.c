95f6766d03a83213a36f27a7e89c618d1bb7cf63
ubiracisouza8900-source <ubiracisouza8900@gmail.com>
2026-01-01 23:48:53 +0000

    commit

--- xadrez.c
+++ xadrez.c
@@ -4,7 +4,6 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
@@ -27,6 +26,36 @@ int main() {
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
+    
+    #include <stdio.h>
+int main(){
+        int bispo = 0;
+        int rainha = 0;
+    //movimentaçao da Toree 5 para direita
+    for (int i = 0; i < 5; i++){
+        printf(" Direita\n");
+    }
+    while (bispo < 2) {
+        //movimentaçao do bispo 2 para diagonal direita
+        for (int j = 0; j < 2; j++){
+            printf(" Diagonal Esquerda\n");
+            printf(" Diagonal Direita\n");
+        }
+        bispo++;
+
+    }
+    do 
+    {
+        //movimentaçao da rainha 3 frente, esquerda e direita
+        for (int k = 0; k < 3; k++){
+            printf(" Frente\n");
+            printf("volta\n");
+            printf(" esquerda\n");
+            printf(" Direita\n");
+        }
+        rainha++;
+    } while (rainha < 1);
 
     return 0;
+
 }
